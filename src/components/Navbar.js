9c9a3d2f202b7f6c9973c8b7516e6583b2ad21b2
Ratikant-rout <ratikantrout171@gmail.com>
2024-05-04 17:09:01 +0530

    yes-2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,43 @@ class Navbar extends React.Component {
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            Cartridge
+            Cartidge
           </a>
+          <button
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            className="navbar-toggler"
+            data-bs-target="#navbarNav"
+            data-bs-toggle="collapse"
+            type="button"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a aria-current="page" className="nav-link active" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item">
+                <a aria-disabled="true" className="nav-link disabled">
+                  Disabled
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
